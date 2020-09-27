6badcb09faab2a7dd151b2d293b79fa6456268d1
Yurbol2014 <67430119+Yurbol2014@users.noreply.github.com>
2020-09-27 23:17:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $( document ).ready(function() {
 	$('.header_burger').click(function(event) {
     $('.header_burger,.header-top,.father_burger').toggleClass('active');
-    $('body').toggleClass('lock');
+    // $('body').toggleClass('lock');
 });
+	$(document).mouseup(function(e) {
+        var $target = $(e.target);
+        if ($target.closest('.header_burger,.header-top').length == 0) {
+            $('.father_burger').removeClass('active') ;
+        }
+
+        $('body').toggleClass('lock');
+    });
+
 	});
 
+ 
